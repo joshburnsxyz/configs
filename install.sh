79d8c9a8ffcdbb708e2820536c1983f6b4310ca0
Josh Burns <joshyburnss@gmail.com>
2022-02-12 00:06:50 +1100

    add skips if directories already exist

--- install.sh
+++ install.sh
@@ -5,7 +5,7 @@ sudo pacman -Syyu --noconfirm
 
 # Read programs.csv and install programs
 while IFS=, read -r install_type name description; do 
-    case $type
+    case $type in
       p)
         sudo pacman -S $name --noconfirm
         ;;
@@ -20,7 +20,15 @@ while IFS=, read -r install_type name description; do
 done < programs.csv
 
 # Install OhMyZsh
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+if [ ! -d "~/.oh-my-zsh" ]; then
+    echo "Oh-My-ZSH Already installed...skipping"
+else
+     sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+fi
 
 # Install emacs config
-git clone https://github.com/joshburnsxyz/.emacs.d ~/.emacs.d
+if [ ! -d "~/.emacs.d" ]; then
+    echo "Emacs config already exists...skipping"
+else
+    git clone https://github.com/joshburnsxyz/.emacs.d ~/.emacs.d
+fi
