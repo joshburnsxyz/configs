e0066b39866ce72d4c961c697979a18cbb5c011f
Josh Burns <joshyburnss@gmail.com>
2022-08-26 01:05:19 +1000

    update install script to use stow

--- install.sh
+++ install.sh
@@ -1,21 +1,12 @@
 #!/bin/sh
-
-install_to_home() {
-  ln -s $(pwd)/$1/* $HOME/
-}
-
 echo "setting macos defaults"
 . ./macos/.macos
 
 echo "install homebrew"
-echo "coming soon...."
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
 echo "installing homebrew packages"
 brew bundle ./macos/Brewfile
 
-echo "installing spacemacs configuration files"
-git clone https://github.com/syl20bnr/spacemacs ~/.emacs.d
-# TODO: install_to_home emacs
-
-echo "installing zsh config"
-install_to_home zsh
+stow -t $HOME/.config nvim
+stow -t $HOME zsh
