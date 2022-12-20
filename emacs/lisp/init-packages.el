5106bccb47e9d8f1c63677c525a563873a39c475
Josh Burns <joshyburnss@gmail.com>
2022-12-20 23:01:43 +1100

    snippets

--- emacs/lisp/init-packages.el
+++ emacs/lisp/init-packages.el
@@ -50,4 +50,12 @@
 (use-package lsp-ui :commands lsp-ui-mode)
 (use-package helm-lsp :commands helm-lsp-workspace-symbol)
 
+;; YASnippet - Snippets and text templating system
+(use-package yasnippet
+  :ensure t
+  :config
+  (yas-global-mode 1))
+(use-package yasnippet-snippets
+  :ensure t)
+
 (provide 'init-packages)
