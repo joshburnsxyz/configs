8e401f537b49b9020813ef401963277fa6802a62
Josh Burns <joshyburnss@gmail.com>
2022-12-20 22:45:22 +1100

    company-mode autocomplete

--- emacs/lisp/init-packages.el
+++ emacs/lisp/init-packages.el
@@ -35,4 +35,13 @@
 (use-package general
   :ensure t)
 
+;; Company mode - Autocomplete
+(use-package company
+  :ensure t
+  :config
+  (add-hook 'after-init-hook 'global-company-mode))
+(use-package company-box
+  :ensure t
+  :hook (company-mode . company-box-mode))
+
 (provide 'init-packages)
