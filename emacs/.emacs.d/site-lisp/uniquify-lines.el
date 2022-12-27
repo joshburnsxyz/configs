4afafd9066b45342985195a1eb7cccd4829a1ca2
Josh Burns <joshyburnss@gmail.com>
2022-12-27 20:51:49 +1100

    lisp functions to filter duplicate lines

---
+++ emacs/.emacs.d/site-lisp/uniquify-lines.el
@@ -0,0 +1,39 @@
+;;; uniquify-lines.el --- Utility functions to filter duplicate lines from files.
+
+;; Copyright (C) 2022  Josh Burns <@joshburnsxyz>.
+
+;; Author: Josh Burns <@joshburnsxyz>
+;; Keywords: extensions
+
+;; This file is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation; either version 2, or (at your option)
+;; any later version.
+
+;; This file is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;;; Commentary:
+
+;;; Code:
+
+(defun my/uniquify-all-lines-region (start end)
+  "Find duplicate lines in region START to END keeping first occurrence."
+  (interactive "*r")
+  (save-excursion
+    (let ((end (copy-marker end)))
+      (while
+          (progn
+            (goto-char start)
+            (re-search-forward "^\\(.*\\)\n\\(\\(.*\n\\)*\\)\\1\n" end t))
+        (replace-match "\\1\n\\2")))))
+
+(defun my/uniquify-all-lines-buffer ()
+  "Delete duplicate lines in buffer and keep first occurrence."
+  (interactive "*")
+  (uniquify-all-lines-region (point-min) (point-max)))
+
+(provide 'uniquify-lines)
+;;; uniquify-lines.el ends here
