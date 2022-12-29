ae9d4e69fa7914e262aeddd56ddc5aba2f4ab78d
Josh Burns <joshyburnss@gmail.com>
2022-12-29 16:24:09 +1100

    delete plugins

--- zsh/.zsh-plugins/perl.plugin.zsh
+++
@@ -1,5 +0,0 @@
-PATH="/Users/jb/perl5/bin${PATH:+:${PATH}}"; export PATH;
-PERL5LIB="/Users/jb/perl5/lib/perl5${PERL5LIB:+:${PERL5LIB}}"; export PERL5LIB;
-PERL_LOCAL_LIB_ROOT="/Users/jb/perl5${PERL_LOCAL_LIB_ROOT:+:${PERL_LOCAL_LIB_ROOT}}"; export PERL_LOCAL_LIB_ROOT;
-PERL_MB_OPT="--install_base \"/Users/jb/perl5\""; export PERL_MB_OPT;
-PERL_MM_OPT="INSTALL_BASE=/Users/jb/perl5"; export PERL_MM_OPT;
