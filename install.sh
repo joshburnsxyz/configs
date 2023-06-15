bc80dcc4245fc4dbee97893590cad528573b502e
Josh Burns <joshyburnss@gmail.com>
2023-06-15 23:41:11 +1000

    update install.sh

--- install.sh
+++ install.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 echo "setting macos defaults"
-. ./macos/.macos
+. ./.macos
 
 echo "install homebrew"
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
 echo "installing homebrew packages"
-brew bundle ./macos/Brewfile
+brew bundle ./Brewfile
 
 stow -t $HOME zsh
 stow -t $HOME emacs
