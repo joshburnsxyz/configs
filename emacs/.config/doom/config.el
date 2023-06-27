1624c7f989e5ce3872df08d4d8d7eea4321788ec
Josh Burns <joshyburnss@gmail.com>
2023-06-27 23:25:13 +1000

    update settings

--- emacs/.config/doom/config.el
+++ emacs/.config/doom/config.el
@@ -29,14 +29,16 @@
 ;; refresh your font settings. If Emacs still can't find your font, it likely
 ;; wasn't installed correctly. Font issues are rarely Doom issues!
 
+(setq doom-font (font-spec :family "FiraMono Nerd Font Mono" :size 12 :weight 'normal))
+
 ;; There are two ways to load a theme. Both assume the theme is installed and
 ;; available. You can either set `doom-theme' or manually load a theme with the
 ;; `load-theme' function. This is the default:
-(setq doom-theme 'doom-molokai)
+(setq doom-theme 'doom-1337)
 
 ;; This determines the style of line numbers in effect. If set to `nil', line
 ;; numbers are disabled. For relative line numbers, set this to `relative'.
-(setq display-line-numbers-type t)
+(setq display-line-numbers-type 'relative)
 
 ;; If you use `org' and don't want your org files in the default location below,
 ;; change `org-directory'. It must be set before org loads!
